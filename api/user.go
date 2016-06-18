e498ed581c729c5a1c1a4defad56d05c8ecc6f15
root <root@iron.engine.kdm.wcss.pl>
2016-06-19 01:29:53 +0200

    more debug

--- api/user.go
+++ api/user.go
@@ -2037,23 +2037,25 @@ func AuthorizeOAuthUser(service, code, state, redirectUri string) (io.ReadCloser
 	req.Header.Set("Accept", "application/json")
 
 	var ar *model.AccessResponse
+	var body string
 	if resp, err := client.Do(req); err != nil {
 		return nil, nil, nil, model.NewLocAppError("AuthorizeOAuthUser", "api.user.authorize_oauth_user.token_failed.app_error", nil, err.Error())
 	} else {
 		ar = model.AccessResponseFromJson(resp.Body)
+		bodyresp, _ := ioutil.ReadAll(resp.Body)
+		body = string(bodyresp)
+
 		if ar == nil {
 			return nil, nil, nil, model.NewLocAppError("AuthorizeOAuthUser", "api.user.authorize_oauth_user.bad_response.app_error", nil, "")
 		}
 	}
 
 	if strings.ToLower(ar.TokenType) != model.ACCESS_TOKEN_TYPE {
-		body, _ := ioutil.ReadAll(resp.Body); 
-		return nil, nil, nil, model.NewLocAppError("AuthorizeOAuthUser", "api.user.authorize_oauth_user.bad_token.app_error", nil, "token_type="+ar.TokenType+". json: "+ar.ToJson()+" body = "+string(body))
+		return nil, nil, nil, model.NewLocAppError("AuthorizeOAuthUser", "api.user.authorize_oauth_user.bad_token.app_error", nil, "token_type="+ar.TokenType+". json: "+ar.ToJson()+" body = "+body)
 	}
 
 	if len(ar.AccessToken) == 0 {
-		body, _ := ioutil.ReadAll(resp.Body); 
-		return nil, nil, nil, model.NewLocAppError("AuthorizeOAuthUser", "api.user.authorize_oauth_user.missing.app_error", nil, "json: "+ar.ToJson()+" body = "+string(body))
+		return nil, nil, nil, model.NewLocAppError("AuthorizeOAuthUser", "api.user.authorize_oauth_user.missing.app_error", nil, "json: "+ar.ToJson()+" body = "+body)
 	}
 
 	p = url.Values{}
